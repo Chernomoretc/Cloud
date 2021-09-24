5f5c1f94ddfb19e2c09c24f4beadcbe073dd7a7f
Stas Chernomoretc <stas.chernomoretc@gmail.com>
2021-09-24 08:58:42 +0300

    lesson1

--- Client/src/main/java/Controller.java
+++ Client/src/main/java/Controller.java
@@ -7,39 +7,40 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.awt.*;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class Controller implements Initializable {
     private static final Logger log = LoggerFactory.getLogger(Controller.class);
-    public ListView<String> listView;
+    public ListView<String> listViewClient;
     public TextField input;
+    public ListView<String> listViewServer;
 
+    private static byte[] buffer = new byte[1024];
+    private String dirClient = "Client/root/";
     private DataInputStream is;
     private DataOutputStream os;
     private FileInputStream fis;
 
 
     public void send(ActionEvent actionEvent) throws Exception {
-        String msg = input.getText();
+        String fileName = input.getText();
         input.clear();
-        fis = new FileInputStream(msg);
-        int size =  fis.read();
-        byte [] bytes  = new byte[size];
-        fis.read(bytes);
-        os.write(bytes);
-        os.flush();
+        sendFile(fileName);
+        listViewServer.getItems().clear();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         try {
+            showCurrentDir();
             Socket socket = new Socket("localhost", 8189);
             is = new DataInputStream(socket.getInputStream());
             os = new DataOutputStream(socket.getOutputStream());
@@ -47,9 +48,11 @@ public class Controller implements Initializable {
             {
                 try {
                     while (true) {
+
                         String msg = is.readUTF();
+                        Platform.runLater(() -> listViewServer.getItems().add(msg));
                         log.debug("received: {}", msg);
-                        Platform.runLater(() -> listView.getItems().add(msg));
+
                     }
                 } catch (Exception e) {
                     log.error("exception while read from input stream");
@@ -57,8 +60,39 @@ public class Controller implements Initializable {
 
 
             });
+            demon.start();
         } catch (IOException e) {
             log.error("e= ", e);
         }
     }
+
+    private void showCurrentDir() throws IOException {
+        listViewClient.getItems().addAll(
+                Files.list(Paths.get(dirClient))
+                        .map(p -> p.getFileName().toString())
+                        .collect(Collectors.toList())
+        );
+    }
+
+    private void sendFile(String fileName) throws IOException {
+        Path file = Paths.get(dirClient, fileName);
+        if (Files.exists(file)) {
+            long size = Files.size(file);
+
+            os.writeUTF(fileName);
+            os.writeLong(size);
+
+            InputStream fileStream = Files.newInputStream(file);
+            int read;
+            while ((read = fileStream.read(buffer)) != -1) {
+                os.write(buffer, 0, read);
+            }
+            os.flush();
+        } else {
+            os.writeUTF(fileName);
+            os.flush();
+        }
+    }
 }
+
+
--- Server/src/main/java/Handler.java
+++ Server/src/main/java/Handler.java
@@ -1,15 +1,19 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Handler implements Runnable {
     private final Socket socket;
     private static final Logger log = LoggerFactory.getLogger(Server.class);
+    private String dirServer = "Server/root/";
+    private static final int BUFFER_SIZE = 256;
+    private final byte[] buffer = new byte[BUFFER_SIZE];
 
 
     public Handler(Socket socket) {
@@ -23,23 +27,48 @@ public class Handler implements Runnable {
     @Override
     public void run() {
         try (DataInputStream is = new DataInputStream(socket.getInputStream());
-             DataOutputStream os = new DataOutputStream(socket.getOutputStream());
-             FileOutputStream fos = new FileOutputStream("server/root/test.txt"))
-        {
-while (true)
-{
-    File file = new File("server/root/test.txt");
-    int size = is.read();
-    byte [] bytes = new byte[size];
-    is.read(bytes);
-    log.debug("Received: {}");
-    fos.write(bytes);
-    os.flush();
+             DataOutputStream os = new DataOutputStream(socket.getOutputStream()))
+             {
+             sendShowServerDir(os);
 
-}
+            while (true) {
+                String fileName = is.readUTF();
+                log.debug("Received fileName: {}", fileName);
+                long size = is.readLong();
+                log.debug("File size: {}", size);
+                int read;
+                try (OutputStream fos = Files.newOutputStream(Paths.get(dirServer, fileName))) {
+                    for (int i = 0; i < (size + BUFFER_SIZE - 1) / BUFFER_SIZE; i++) {
+                        read = is.read(buffer);
+                        fos.write(buffer, 0 , read);
+                    }
+                    sendShowServerDir(os);
+                } catch (Exception e) {
+                    log.error("problem with file system");
+                }
+               // os.writeUTF("OK");
+
+
+            }
         } catch (Exception e) {
-            log.error("stacktrace: ",e);
+            log.error("stacktrace: ", e);
         }
 
+
+    }
+
+    private void sendShowServerDir(DataOutputStream os) {
+        try {
+            List<String> showServerDir = Files.list(Paths.get(dirServer))
+                    .map(p -> p.getFileName().toString())
+                    .collect(Collectors.toList());
+
+            for (String s : showServerDir) {
+                os.writeUTF(s);
+                os.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
